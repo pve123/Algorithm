1279a44d069e9901b199b7380cfccfe5fcf124b2
pve123 <tkddlfdlfemd@naver.com>
2020-04-08 21:58:04 +0900

    Algorithm

--- src/그래프/BFS_Graph.java
+++ src/그래프/BFS_Graph.java
@@ -20,7 +20,7 @@ public class BFS_Graph {
 		link[v].add(w);
 	}
 
-	/** s를 시작 노드으로 한 BFS로 탐색하면서 탐색한 노드들을 출력 */
+	/** v를 시작 노드으로 한 BFS로 탐색하면서 탐색한 노드들을 출력 */
 	void BFS(int v) {
 		// 노드의 방문 여부 판단 (초깃값: false)
 		boolean visited[] = new boolean[V];
@@ -35,7 +35,7 @@ public class BFS_Graph {
 		while (queue.size() != 0) {
 			// 방문한 노드를 큐에서 추출(dequeue)하고 값을 출력
 			v = queue.poll();
-			System.out.print(v + " ");
+			System.out.printf("%3d", v);
 
 			// 방문한 노드와 인접한 모든 노드를 가져온다.
 			Iterator<Integer> i = link[v].listIterator();
@@ -55,7 +55,7 @@ public class BFS_Graph {
 		BFS_Graph g = new BFS_Graph(4);
 		System.out.println("============ 너비우선 탐색 ============");
 		for (int i = 0; i < g.V; i++) {
-			System.out.print("정점 " + i + "번 부터 탐색 : ");
+			System.out.printf("정점 " + i + "번 부터 탐색 : ");
 			g.addEdge(0, 1);
 			g.addEdge(0, 2);
 			g.addEdge(1, 2);
